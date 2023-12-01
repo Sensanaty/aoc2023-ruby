4a8031f3abc6cf03a1f192ab2b14509016efe3d9
Luka <salevic@lu-ka.me>
2023-12-01 17:09:37 +0100

    Add part 1 solution

---
+++ days/1/main.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+FILE_PATH = File.join(File.dirname(__FILE__), 'input')
+
+### === Part 1 === ###
+def trebuchet_calibration
+  input = File.read(FILE_PATH)
+
+  numbers = input.split("\n").map do |line|
+    scan = line.scan(/\d/)
+
+    (scan[0] + scan[-1]).to_i
+  end
+
+  numbers.inject(:+)
+end
+
+p trebuchet_calibration
