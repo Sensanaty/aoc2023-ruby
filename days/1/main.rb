bb6c1c3d871b54b00c7b0937e6246c86991b51e7
Luka <salevic@lu-ka.me>
2023-12-02 11:36:08 +0100

    Change inject to sum
    It's faster, apparently!
    
    https://stackoverflow.com/questions/41449617/why-is-sum-so-much-faster-than-inject

--- days/1/main.rb
+++ days/1/main.rb
@@ -5,7 +5,7 @@ INPUT = File.read(FILE_PATH)
 
 ### === Part 1 === ###
 # Splits the input file, which is delimited by newlines, and scans for any digits per line. The scan returns an array of
-# digits, so we simply return the 1st index position and the last index position, after which we run a simple inject
+# digits, so we simply return the 1st index position and the last index position, after which we simply sum the result
 def trebuchet_calibration
   numbers = INPUT.split("\n").map do |line|
     scan = line.scan(/\d/)
@@ -13,7 +13,7 @@ def trebuchet_calibration
     (scan[0] + scan[-1]).to_i
   end
 
-  numbers.inject(:+)
+  numbers.sum
 end
 
 puts "Part 1 solution: #{trebuchet_calibration}"
